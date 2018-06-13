176d3c89fc9ff4a4c1bcb6fa9d37f468e647106c
aeroniemi <flightcontrol222@gmail.com>
2018-06-13 20:27:04 +0100

    How about some buttons that work?

--- plugins/SASL/data/modules/displays/mfd/B787MFDcontrols.lua
+++ plugins/SASL/data/modules/displays/mfd/B787MFDcontrols.lua
@@ -5,11 +5,41 @@ local shortButtonImage = loadImage("images/arrowWidthButton.png")
 local shortButtonInvertedImage = loadImage("images/arrowWidthButtonInverted.png")
 function draw()
     drawAll(components)
-    sasl.gl.drawTexture(longButtonInvertedImage, 10, 895, 252, 65)
-    sasl.gl.drawTexture(longButtonImage, 262, 895, 252, 65)
-    sasl.gl.drawTexture(shortButtonImage, 534, 895, 93, 65)
-    sasl.gl.drawText(fontLato, 131, 915, "MAP", 35, false, false, TEXT_ALIGN_CENTER, colourWhite)
-    sasl.gl.drawText(fontLato, 383, 915, "PLAN", 35, false, false, TEXT_ALIGN_CENTER, colourWhite)
-    sasl.gl.drawText(fontLato, 580.5, 933, "MENU", 25, false, false, TEXT_ALIGN_CENTER, colourWhite)
-    sasl.gl.drawTriangle(580.5, 910, 590.5, 920, 570.5, 920, colourWhite)
+    if mfdLeft == 1 then
+        sasl.gl.drawTexture(longButtonInvertedImage, 10, 900, 242, 60)
+        sasl.gl.drawTexture(longButtonImage, 252, 900, 242, 60)
+    elseif mfdLeft == 2 then
+        sasl.gl.drawTexture(longButtonImage, 10, 900, 242, 60)
+        sasl.gl.drawTexture(longButtonInvertedImage, 252, 900, 242, 60)
+    end
+    sasl.gl.drawTexture(shortButtonImage, 529, 900, 93, 60)
+    sasl.gl.drawText(fontLato, 131, 917, "MAP", 35, false, false, TEXT_ALIGN_CENTER, colourWhite)
+    sasl.gl.drawText(fontLato, 383, 917, "PLAN", 35, false, false, TEXT_ALIGN_CENTER, colourWhite)
+    sasl.gl.drawText(fontLato, 575, 933, "MENU", 25, false, false, TEXT_ALIGN_CENTER, colourWhite)
+    if mfdLeftMenu1 == 0 then
+        sasl.gl.drawTriangle(575, 920, 585, 910, 565, 910, colourWhite)
+    else
+        sasl.gl.drawRectangle(499, 400, 131, 500, colourGrey)
+        sasl.gl.drawTriangle(575, 910, 585, 920, 565, 920, colourWhite)
+    end
+end
+
+function onMouseDown(component, x, y, button, parentX, parentY)
+    -- set MAP mode
+    if button == MB_LEFT and x >= 10 and x <= 252 and y >= 900 and y <= 960 then
+        mfdLeft = 1
+    end
+    -- set PLAN mode
+    if button == MB_LEFT and x > 252 and x <= 504 and y >= 900 and y <= 960 then
+        mfdLeft = 2
+    end
+    -- set MENU mode
+    if button == MB_LEFT and x >= 529 and x <= 632 and y >= 900 and y <= 960 then
+        if mfdLeftMenu1 == 1 then
+            mfdLeftMenu1 = 0
+        else
+            mfdLeftMenu1 = 1
+        end
+    end
+    return true
 end
--- plugins/SASL/data/modules/displays/mfd/B787MFDcore.lua
+++ plugins/SASL/data/modules/displays/mfd/B787MFDcore.lua
@@ -23,46 +23,24 @@ local dateDay = globalPropertyi("sim/cockpit2/clock_timer/current_day")
 
 local xpdrCode = globalPropertyi("sim/cockpit2/radios/actuators/transponder_code")
 local com1 = globalPropertyi("sim/cockpit2/radios/actuators/com1_frequency_hz")
-
-function draw()
-    --sasl.gl.setClipArea(0, 0, 300, 200)
-    --sasl.gl.drawRectangle(0, 0, 300, 200, colourWhite)
-    --[[local offsetY = (get(pitch)*-2)-100
-    local ias = math.floor(get(indicatedAS))
-    local altitude = 35000 
-    --math.floor(get(altitudeAS))
-    sasl.gl.drawRotatedTexture (attIndImage, get(roll), 0, offsetY, 600, 400, colourWhite)
-    sasl.gl.drawTexture(overlayImage, 60, 38, 180, 150)
-    sasl.gl.drawText(fontLatoBold , 72, 167, ias, 10, false , false , TEXT_ALIGN_CENTER , colourPink)
-    sasl.gl.drawText(fontLatoBold , 228, 169, altitude, 8, false , false , TEXT_ALIGN_CENTER , colourPink)
-    
-    end
---]]
-    --sasl.gl.setRenderTarget(pfdDisplayTx, true)
-    ----------------------------------------------------------------------------------
-    -- Clock timer
-    ----------------------------------------------------------------------------------
-    sasl.gl.drawRectangle(638, 0, 4, 960, colourGrey)
-
-    drawAll(components)
-end
-local conponent = {
-    B787MFDfuel {position = {1001, 0, 279, 200}},
-    B787MFDengine {position = {641, 0, 638, 960}}
-}
-
 function update()
     updateAll(components)
 end
+function draw()
+    drawAll(components)
+    sasl.gl.drawRectangle(638, 0, 4, 960, colourGrey)
+end
+
 components = {
     B787MFDfuel {position = {1001, 0, 279, 200}},
     B787MFDengine {position = {641, 0, 638, 960}}
 }
+
 if get(enableExperimentalDisplays) == 1 then
     components = {
         B787MFDfuel {position = {1001, 0, 279, 200}},
         B787MFDengine {position = {641, 0, 638, 960}},
-        B787MFDcontrols {position = {0, 0, 638, 960}},
-        B787MFDnav {position = {0, 0, 638, 960}, clip = true}
+        B787MFDnav {position = {0, 0, 638, 960}, clip = true},
+        B787MFDcontrols {position = {0, 0, 638, 960}}
     }
 end
